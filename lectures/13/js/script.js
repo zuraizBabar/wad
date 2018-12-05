594307aca0ee1715afb50d54611509530c9d006b
Muhammad Ali Makhdoom <14muhammad@gmail.com>
2018-12-05 18:29:15 +0500

    Quiz 4 Solution

--- lectures/13/js/script.js
+++ lectures/13/js/script.js
@@ -1,4 +1,5 @@
-var questions = [{
+var questions = [
+    {
     question : "When a user views a page containing a JavaScript program, which machine actually executes the script?",
     choices : [ "The User's machine running a Web browser",
         "The Web server",
@@ -27,11 +28,50 @@ var quizOver = false;
 displayCurrentQuestion();
 document.getElementById("quiz-message").style.display = 'none';
 function displayNext() {
-    /*Write your code here */
+    if(!quizOver){
+        var selectedValue = null;
+        if(document.querySelector('input[name="dq"]:checked') !== null)
+            selectedValue = document.querySelector('input[name="dq"]:checked').value;
+        if (selectedValue == null) {
+            document.getElementById("quiz-message").innerText = "Please selected an answer"
+            document.getElementById("quiz-message").style.display = 'block';
+        } else{
+            document.getElementById("quiz-message").style.display = 'none';
+            if(selectedValue == questions[currentQuestion].correctAnswer){
+                correctAnswers++;
+            }
+            currentQuestion++;
+            if(currentQuestion < questions.length){
+                displayCurrentQuestion();
+            }
+            else {
+                displayScore();
+                document.getElementById("next-btn").innerText = "Play Again?"
+                quizOver = true;
+            }
+        }
+    } else {
+        quizOver = false;
+        document.getElementById("next-btn").innerText = "Next Question";
+        resetQuiz();
+        displayCurrentQuestion();
+        hideScore();
+    }
 }
 
 function displayCurrentQuestion() {
-    /*Write your code here */
+    var question = questions[currentQuestion].question;
+    var questionId = document.getElementById("question");
+    var choiceList = document.getElementById("choice-list");
+    var numChoices = questions[currentQuestion].choices.length;
+
+    questionId.innerText = question;
+    choiceList.innerHTML = "";
+    var choice;
+    for(var i=0; i<numChoices; i++){
+        choice = questions[currentQuestion].choices[i];
+        choiceList.innerHTML += "<li><input type='radio' value='"+i+"' name='dq'>" + choice + "</li>";
+    }
 }
 
 function resetQuiz() {
