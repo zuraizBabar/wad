bdf95d04386394006294d5b03528afcdd0c970f6
Muhammad Ali Makhdoom <14muhammad@gmail.com>
2019-01-08 10:24:18 +0500

    products table with few products in db init

--- pro/database/tech_box_db.sql
+++ pro/database/tech_box_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 02, 2019 at 05:55 PM
+-- Generation Time: Jan 08, 2019 at 06:22 AM
 -- Server version: 10.1.32-MariaDB
 -- PHP Version: 7.2.5
 
@@ -17,6 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
+
 --
 -- Database: `tech_box_db`
 --
@@ -41,7 +42,6 @@ INSERT INTO `brands` (`brand_id`, `brand_title`) VALUES
 (2, 'Dell'),
 (3, 'Samsung'),
 (4, 'Toshiba'),
-(5, 'HP'),
 (6, 'Oppo'),
 (7, 'Sony');
 
@@ -67,7 +67,37 @@ INSERT INTO `categories` (`cat_id`, `cat_title`) VALUES
 (6, 'Watches'),
 (7, 'Cameras'),
 (8, 'iPads'),
-(9, 'Projectors');
+(9, 'Projectors'),
+(10, 'Mobiles');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `pro_id` int(10) NOT NULL,
+  `pro_cat` int(10) NOT NULL,
+  `pro_brand` int(10) NOT NULL,
+  `pro_title` varchar(200) NOT NULL,
+  `pro_price` int(10) NOT NULL,
+  `pro_desc` varchar(2000) NOT NULL,
+  `pro_image` varchar(300) NOT NULL,
+  `pro_keywords` varchar(300) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`pro_id`, `pro_cat`, `pro_brand`, `pro_title`, `pro_price`, `pro_desc`, `pro_image`, `pro_keywords`) VALUES
+(5, 1, 1, 'apple Mac Book', 323000, '', '71KyjhnQY4L._SY355_.jpg', '3212'),
+(6, 1, 2, 'dell inpiron', 50000, 'dell is good laptop', 'dell1.jpg', 'dell, inspiron'),
+(10, 10, 1, 'iPhone 7', 90000, 'Apple product', '71x3e0x+M2L._SX569_.jpg', 'apple phone, iphone 7'),
+(11, 7, 7, 'Sony Alpha a58', 59900, 'Sony is Good Company', 'song1.jpg', 'sony , camera, DSLR'),
+(12, 6, 3, 'Samsung Gear S3', 20000, 'Samsung product', 'samsung2 (2).jpg', 'samsung, watch'),
+(13, 10, 6, 'oppo f1s', 25000, 'Oppo phone', 'QMobile-Noir-i8i-Techjuice.jpg', 'oppo');
 
 --
 -- Indexes for dumped tables
@@ -86,6 +116,12 @@ ALTER TABLE `categories`
   ADD PRIMARY KEY (`cat_id`);
 
 --
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`pro_id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -99,7 +135,13 @@ ALTER TABLE `brands`
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `cat_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `cat_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `pro_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
