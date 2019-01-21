35aa3a33ab87239cd5fa79ed009fad7cbf827d0f
Muhammad Ali Makhdoom <14muhammad@gmail.com>
2019-01-21 23:17:09 +0500

    admin panel home page init with sidebar

---
+++ pro/admin/index.php
@@ -0,0 +1,65 @@
+<?php
+require_once "db_connection.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="">
+    <link rel="stylesheet" type="text/css" href="../css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="css/custom.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Bangers|Old+Standard+TT">
+    <title>TechBox Admin Panel</title>
+    <style>
+        * {
+            font-family: 'Old Standard TT', serif;
+        }
+    </style>
+</head>
+<body>
+<div class="wrapper">
+    <nav id="sidebar">
+        <div class="sidebar-header">
+            <h3><img src="../media/logo.png"> Admin Panel</h3>
+        </div>
+        <ul class="list-unstyled components">
+            <li>
+                <a href="index.php?insert_product">
+                    <i class="fas fa-plus"></i> Insert New Product
+                </a>
+            </li>
+        </ul>
+    </nav>
+    <div id="content">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+                <button type="button" id="sidebarCollapse" class="btn btn-info">
+                    <i class="fas fa-bars"></i>
+                </button>
+            </div>
+        </nav>
+        <div class="container">
+            <?php
+            if(isset($_GET['insert_product'])){
+                include ('insert_product.php');
+            }
+            ?>
+        </div>
+    </div>
+</div>
+<script src="../js/jquery-3.3.1.js"></script>
+<script src="../js/bootstrap.bundle.js"></script>
+<script>
+    $(document).ready(function () {
+        $('#sidebarCollapse').on('click', function () {
+            $('#sidebar').toggleClass('active');
+        });
+
+    });
+</script>
+</body>
+</html>
