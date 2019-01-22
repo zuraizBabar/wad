c2faef631f95c7cdcb086ac4a5fff3f48affa1a9
zuraizBabar <420zuraiz9211@ucp.edu.pk>
2019-01-22 20:21:43 +0500

    Wad New class work sessions

--- pro/admin/insert_brand.php
+++ pro/admin/insert_brand.php
@@ -1,6 +1,8 @@
 <?php
 require_once "db_connection.php";
-
+if(!isset($_SESSION['user_email'])){
+    header('location: login.php?not_admin=You are not Admin!');
+}
 if(isset($_POST['insert_btn'])) {
     $pro_title = $_POST['cat_title'];
     $Query = "insert into Brands(Brands_Title)
