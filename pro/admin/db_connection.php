1a48f8a57e8361f2be4e53c1ee9e46c183de1060
zuraizBabar <420zuraiz9211@ucp.edu.pk>
2019-01-22 18:17:54 +0500

    del my own data to get sir data

--- pro/admin/db_connection.php
+++
@@ -1,4 +0,0 @@
-<?php
-$con = mysqli_connect("localhost","root","","phpFirstTask");
-if(!$con)
-    die("Connection failed");
--- pro/server/db_connection.php
+++
@@ -1,4 +0,0 @@
-<?php
-$con = mysqli_connect("localhost","root","","tech_box_db");
-if(!$con)
-    die("Connection failed");
