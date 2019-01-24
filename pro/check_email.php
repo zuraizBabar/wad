f1e8fd6fe3551eda3408e9163555864066816c81
Muhammad Ali Makhdoom <14muhammad@gmail.com>
2019-01-24 10:05:29 +0500

    email hint using ajax

---
+++ pro/check_email.php
@@ -0,0 +1,8 @@
+<?php
+require "server/functions.php";
+$e = $_REQUEST["e"];
+$sel_email = "select * from customers where cust_email= '$e'";
+$run_email  = mysqli_query($con,$sel_email);
+$count = mysqli_num_rows($run_email);
+if($count>0)
+    echo "Already registered";
--- pro/register.php
+++ pro/register.php
@@ -11,6 +11,24 @@ require "server/functions.php";
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Bangers|Old+Standard+TT">
+    <script>
+        function checkEmail(str) {
+            if (str.length == 0) {
+                document.getElementById("hint").innerHTML = "";
+                return;
+            } else {
+                var xmlhttp = new XMLHttpRequest();
+                xmlhttp.onreadystatechange = function() {
+                    if (this.readyState == 4 && this.status == 200) {
+                        document.getElementById("hint").innerHTML = this.responseText;
+                    }
+                };
+                xmlhttp.open("GET", "check_email.php?e=" + str, true);
+                xmlhttp.send();
+                //document.getElementById('hint').innerHTML = 'loading...';
+            }
+        }
+    </script>
 </head>
 <body>
 
@@ -117,7 +135,9 @@ require "server/functions.php";
                         <div class="input-group-prepend">
                             <div class="input-group-text"><i class="fas fa-at"></i></div>
                         </div>
-                        <input type="text" class="form-control" id="c_email" name="c_email" placeholder="Enter your email" >
+                        <input type="text" class="form-control" id="c_email" name="c_email" placeholder="Enter your email"
+                               onkeyup="checkEmail(this.value)">
+                        <span class="text-danger" id="hint"></span>
                     </div>
                 </div>
             </div>
